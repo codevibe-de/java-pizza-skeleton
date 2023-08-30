883aac4dfc1dbf290735f6f10d8a930c02af7c45
Thomas Auinger <thomas@auinger.de>
2023-08-30 12:01:15 +0200

    javadoc and jetbrains annotations

--- src/main/java/pizza/product/ProductRepository.java
+++ src/main/java/pizza/product/ProductRepository.java
@@ -3,6 +3,9 @@ package pizza.product;
 import java.util.Collection;
 import java.util.Optional;
 
+/**
+ * ProductRepository offers persistence access methods to services.
+ */
 public interface ProductRepository {
 
     Product save(Product product);
--- src/main/java/pizza/product/ProductService.java
+++ src/main/java/pizza/product/ProductService.java
@@ -1,5 +1,7 @@
 package pizza.product;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Map;
 
 public class ProductService {
@@ -18,18 +20,31 @@ public class ProductService {
     // business logic
     //
 
+    /**
+     * getProduct returns the {@link Product} instance for the given productId or throws a
+     * {@link ProductNotFoundException} if it doesn't exist.
+     * @throws ProductNotFoundException if there is no product for the provided id
+     * @return a {@link Product} instance
+     */
+    @NotNull
     public Product getProduct(String productId) {
         return null; // fixme
     }
 
-    public Double getTotalPrice(Map<String, Integer> productQuantities) {
+    /**
+     * @param productQuantities a mapping of productIds to their respective order quantity
+     * @return the order's total price
+     */
+    public double getTotalPrice(Map<String, Integer> productQuantities) {
         double result = 0.0;
         // TODO implement calculation
         return result;
     }
 
-    public Product createProduct(Product product) {
+    /**
+     * @param product the product to be added to persistence, which must contain an id already.
+     */
+    public void createProduct(Product product) {
         // todo
-        return null;
     }
 }
